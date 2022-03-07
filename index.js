846dd4e8a87622f2f05df82be2afa0f7ee02edba
RoUchiha <swaggsikh@gmail.com>
2022-03-06 18:21:30 -0600

    commit tree e34842e6249df952e35b227842cf2a039a2e231e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+
+function map(array, func) {
+    let newArray = [];
+    for (const int of array) {
+        newArray.push(func(int))
+    }
+    return newArray
+}
+
+
+function reduce(array, func, start) {
+    let value;
+    if (start) {
+        value = start;
+        for (let i = 0; i < array.length; i++) {
+            value = func(array[i], value);
+        }
+        return value;
+    } else {
+        value = array[0];
+        for (let i = 1; i < array.length; i++) {
+            value = func(array[i], value);
+        }
+        return value;
+    }
+}
